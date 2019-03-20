3e6a78d9e0a2af74bdf6b92a4fa8c6a08d0fd7fd
Damyan Petev <damyan.petev@gmail.com>
2019-03-20 20:20:28 +0200

    add tasks chain

--- src/ng-new/index.ts
+++ src/ng-new/index.ts
@@ -1,12 +1,31 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain, schematic } from '@angular-devkit/schematics';
 import { OptionsSchema } from './schema';
-
+import {
+  NodePackageInstallTask,
+  RepositoryInitializerTask,
+} from '@angular-devkit/schematics/tasks';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function newProject(_options: OptionsSchema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    _context.logger.info(`Generating ${_options.name}`);
+export function newProject(options: OptionsSchema): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info(`Generating ${options.name}`);
+
+    
+
+    return chain([
+      // TODO reuse workspace + application and build 
+      schematic('workspace', {}),
+      (tree: Tree, context: SchematicContext) => {
+        if (false) {
+          const installTask = context.addTask(new NodePackageInstallTask(options.name));
+          context.addTask(
+            new RepositoryInitializerTask( options.name ),
+            installTask ? [installTask] : [],
+          );
+        }
+      }
+    ])
     return tree;
   };
 }
