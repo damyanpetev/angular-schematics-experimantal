4be3695f5692168151fff04ff8e49170540e0290
Damyan Petev <damyan.petev@gmail.com>
2019-03-21 10:36:06 +0200

    add rxjs, inquirer, attempt async prompt

--- src/ng-new/index.ts
+++ src/ng-new/index.ts
@@ -4,6 +4,8 @@ import {
   NodePackageInstallTask,
   RepositoryInitializerTask,
 } from '@angular-devkit/schematics/tasks';
+import * as inquirer from 'inquirer';
+import { Observable, defer } from 'rxjs';
 // import { } from '@schematics/angular'; // TODO add as dep?
 
 // You don't have to export the function as default. You can also have more than one rule factory
@@ -15,6 +17,20 @@ export function newProject(options: OptionsSchema): Rule {
     
 
     return chain([
+      (tree: Tree, context: SchematicContext): Observable<Tree> => {
+        // https://medium.com/rocket-fuel/angular-schematics-asynchronous-schematics-dc998c0b6fba
+        // https://medium.com/@benlesh/rxjs-observable-interop-with-promises-and-async-await-bebb05306875
+        return defer<Tree>(async function() {
+          const userInput = await inquirer.prompt({
+            choices: ['Custom', 'Default'],
+            default: 'Custom',
+            message: 'Choose the theme for the project:',
+            name: 'theme',
+            type: 'list'
+          });
+          return tree;
+        })
+      },
       // TODO reuse workspace + application and build?
       externalSchematic('@schematics/angular', 'workspace', { name: options.name }),
       (tree: Tree, context: SchematicContext) => {
