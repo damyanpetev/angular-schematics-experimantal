be5ed1cf380dc700007b2bf4862335518c2e0a35
Damyan Petev <damyan.petev@gmail.com>
2019-03-21 19:21:47 +0200

    add full app creation to chain

--- src/ng-new/index.ts
+++ src/ng-new/index.ts
@@ -1,4 +1,4 @@
-import { Rule, SchematicContext, Tree, chain, externalSchematic } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain, externalSchematic, mergeWith, apply, empty, move, applyTemplates } from '@angular-devkit/schematics';
 import { OptionsSchema } from './schema';
 import {
   NodePackageInstallTask,
@@ -6,7 +6,12 @@ import {
 } from '@angular-devkit/schematics/tasks';
 import * as inquirer from 'inquirer';
 import { Observable, defer } from 'rxjs';
-// import { } from '@schematics/angular'; // TODO add as dep?
+// import { } from '@schematics/angular'; // TODO add as DEV dep for workspace/application options schema?
+
+interface IgxSchematicContext extends SchematicContext {
+  theme: string;
+}
+
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -17,23 +22,36 @@ export function newProject(options: OptionsSchema): Rule {
     
 
     return chain([
-      (tree: Tree, context: SchematicContext): Observable<Tree> => {
+      (tree: Tree, context: IgxSchematicContext): Observable<Tree> => {
         // https://medium.com/rocket-fuel/angular-schematics-asynchronous-schematics-dc998c0b6fba
         // https://medium.com/@benlesh/rxjs-observable-interop-with-promises-and-async-await-bebb05306875
         return defer<Tree>(async function() {
-          const userInput = await inquirer.prompt({
+          const theme = (await inquirer.prompt<{theme: string}>({
             choices: ['Custom', 'Default'],
             default: 'Custom',
             message: 'Choose the theme for the project:',
             name: 'theme',
             type: 'list'
-          });
+          })).theme;
+          context.theme = theme;
+          context.logger.info('');
           return tree;
-        })
+        });
       },
-      // TODO reuse workspace + application and build?
-      externalSchematic('@schematics/angular', 'workspace', { name: options.name }),
-      (tree: Tree, context: SchematicContext) => {
+      // Task chain based on @schematics/angular ng-new schematic
+      mergeWith(
+        apply(empty(), [
+          externalSchematic('@schematics/angular', 'workspace', { name: options.name }),
+          externalSchematic('@schematics/angular','application', { projectRoot: '', name: options.name, skipInstall: true}),
+          applyTemplates({}),
+          (tree: Tree, context: IgxSchematicContext) => {
+            // extend project entry point:
+            tree.create("ignite-ui-cli.json", JSON.stringify({ theme: context.theme }));
+          },
+          move(options.name),
+        ]),
+      ),
+      (tree: Tree, context: IgxSchematicContext) => {
         if (false) {
           const installTask = context.addTask(new NodePackageInstallTask(options.name));
           context.addTask(
